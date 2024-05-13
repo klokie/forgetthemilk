64fccae27b950dad836332f49291139bcbb105a4
Daniel Grossfeld <github@klokie.com>
2024-05-13 21:38:42 +0200

    feature: now parsing all fields from RTM :)

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # forgetthemilk
 
-A small script to convert the JSON exported by Remember The Milk into a CSV format file that Todoist will ingest as a project template
+Convert all tasks exported by Remember The Milk in JSON format into a CSV format file or files that Todoist will ingest as a project template.
+
+## dependencies
+
+- Python 3.6+
+
+## installation
+
+- `python -m venv venv && source venv/bin/activate`
+- `pip install argparse python-dateutil relativedelta python-dateutil`
+
+## Usage
 
 Usage is dirt simple:
 
 ```sh
-$ ./forgetthemilk.py  --help
-usage: forgetthemilk.py [-h] rtm_json csv_result
+$ python ./forgetthemilk.py --help
+usage: forgetthemilk.py [-h] rtm_json csv_incomplete csv_completed
 
 positional arguments:
-  rtm_json    The JSON file you exported from Remember The Milk
-  csv_result  CSV file you want your converted RTM tasks written to
+  rtm_json        The JSON file you exported from Remember The Milk - required
+  csv_incomplete  CSV file to which to write incomplete RTM tasks - optional; defaults to
+                  out.csv
+  csv_completed   CSV file to which to write completed RTM tasks - optional; defaults to
+                  rtm_completed.csv
 
 optional arguments:
   -h, --help  show this help message and exit
@@ -18,6 +32,13 @@ optional arguments:
 
 ## Caveats
 
-This simple script doesn't handle due dates because apparently converting dates from Excel to Python is a nightmare.
+RTM tasks are imported as active tasks in the current project to which they are imported.
 
-I've also chosen default values for priority and indent, obviously adjust as you see fit, and if anyone cares to make this smarter, that would be fantastic!
+Note that you will need to sign up for pro plan to increase project limit from 5.
+Also, mind the limits for 300 active tasks for any given project. This is a limitation of Todoist, and is independent of free vs pro plan.
+
+Labels won't automatically show up in the sidebar. To add them,
+
+1. go to Filters & Labels
+2. Find imported labels under the list at the bottom.
+3. Click on the label you want to show up in the sidebar. You can add it to "Favorites" to show up in the sidebar.
--- forgetthemilk.py
+++ forgetthemilk.py
@@ -1,28 +1,41 @@
+#!/usr/bin/env python3
+
 import json
 import argparse
 from json.decoder import JSONDecodeError
 import csv
 import datetime
+import re
 
-# TODO - might need to split by "project" (RTM list)
-# TODO - make sure to follow CSV template provided by Todoist
-# TODO - make sure completed tasks are imported as such and not as active tasks
-# TODO - need to sign up for pro plan to increase project limit from 5
-# TODO - mind the limits for 300 active tasks
+
+"""
+- make sure to follow CSV template provided by Todoist
+- split by "project" (RTM list)
+- make sure completed tasks are imported as such and not as active tasks
+"""
+
+# NEWLINE = "\\n"
+NEWLINE = "\n"
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "rtm_json", help="The JSON file you exported from Remember The Milk"
+        "rtm_json",
+        help="The JSON file you exported from Remember The Milk",
+        default="rtm.json",
     )
     parser.add_argument(
         "csv_incomplete",
         help="CSV file you want your converted *incomplete* RTM tasks written to",
+        default="out.csv",
+        nargs="?",
     )
     parser.add_argument(
         "csv_completed",
         help="CSV file you want your converted *completed* RTM tasks written to",
+        default="completed.csv",
+        nargs="?",
     )
     return parser.parse_args()
 
@@ -36,6 +49,8 @@ def date_from_int(date_int):
 
 
 def format_date(date_str):
+    if not date_str:
+        return None
     try:
         date_obj = datetime.datetime.strptime(date_str, "%Y-%m-%d")
         return date_obj.strftime("%Y-%m-%d")
@@ -88,28 +103,27 @@ def get_list_from_task(task, lists):
     return result
 
 
-def write_notes(writer, url, date_created, date_modified, location, mylist, tags):
+def write_notes(writer, url, date_created, date_modified, location, list, tags):
     notes = []
-    # TODO add metadata to notes, if set
+    """add metadata to notes, if set"""
     for k, v in {
-        "url": url,
+        # "url": url,
         "created": date_created,
         "modified": date_modified,
-        "location": location,
-        "mylist": mylist,
-        "tags": tags,
+        # "location": location,
+        # "list": list,
+        # "tags": tags,
     }.items():
         if v:
             notes.append(f"{k}: {v}")
 
-    # FIXME insert newlines - maybe requires quotes around the field
-    notes = "; ".join(notes)
+    notes = NEWLINE.join(notes)
 
     if notes:
         # write notes to line after task
         writer.writerow(
             [
-                "Note",  # "TYPE", # Task | Note
+                "note",  # "TYPE", # Task | Note
                 notes,  # "CONTENT"
                 None,  # "DESCRIPTION"
                 None,  # "PRIORITY"
@@ -130,8 +144,9 @@ def get_tags_from_task(task, tags):
         task_tags = [tag for tag in tags if tag.get("tag_id") in task.get("tags", [])]
     except Exception as e:
         print(f"Error processing tags: {e}", task)
-    finally:
-        return ";".join(task_tags)
+        task_tags = []
+
+    return task_tags
 
 
 def compile_notes(notes, task):
@@ -143,53 +158,93 @@ def compile_notes(notes, task):
 
     except Exception as e:
         print(f"Error processing notes: {e}", task)
-    finally:
-        # print("task_notes", task_notes)
-        return task_notes
+        task_notes = []
+
+    # print("task_notes", task_notes)
+    return task_notes
 
 
-def get_description(notes):
-    # FIXME insert newlines - maybe requires quotes around the field
-    return "\\n".join(
+def get_description(url, notes):
+    description = ""
+    if url:
+        description += url + NEWLINE
+    description += NEWLINE.join(
         [
-            note.get("content", "").replace("\n", "\\n").replace("\r", "\\n")
+            note.get("content", "").replace("\n", NEWLINE).replace("\r", NEWLINE)
             for note in notes
         ]
     )
 
-
-def parse_due_date(due_date):
-    if due_date:
-        # TODO parse recurring tasks if "repeat" is not null, in which case it is a semi-colon separated list of:
-        # FREQ=DAILY|MONTHLY|WEEKLY|YEARLY
-        # INTERVAL=1|2|3|4|90
-        # COUNT=4
-        # BYDAY=TH
-        # WKST=SU
-        pass
-
-    return due_date
+    description = replace_urls_with_markdown(description)
+    return description
 
 
-def write_tasks(writer, name, description, due_date, priority, indent=1):
-    # Write task details to CSV
+def replace_urls_with_markdown(text):
+    """Replace all bare URLs in the text with Markdown links, skipping already formatted ones."""
+    # Regex to match URLs that are not already in Markdown link format
+    # This regex looks for URLs that are not preceded by '(' or ']' and not followed by ')'
+    url_pattern = re.compile(r"(?<!\()https?://\S+(?!\))")
+
+    # Replace each URL with its Markdown equivalent
+    def replacement(match):
+        url = match.group(0)
+        clean_url = re.sub(r"^https?://", "", url)  # Strip 'http://' or 'https://'
+        clean_url = re.sub(r"/$", "", clean_url)  # Strip trailing slash
+        clean_url = re.sub(r"^www\.", "", clean_url)  # Strip 'www.'
+        return f"[{clean_url}]({url})"
+
+    # Use re.sub with a function to avoid replacing already formatted URLs
+    return re.sub(url_pattern, replacement, text)
+
+
+def write_tasks(
+    writer, name, description, due_date, priority, duration, duration_unit, indent=1
+):
+    """
+    Write task details to CSV
+    formatted according to Todoist CSV template
+    """
     writer.writerow(
         [
-            # TODO match to Todoist CSV template
-            "Task",  # "TYPE", # Task | Note # TODO write notes to line after task
+            "task",  # "TYPE", # Task | Note # write notes to line after task
             name,  # "CONTENT", # Task Name or Note Content
-            description,  # "DESCRIPTION", # Notes # TODO determine from task notes
-            priority,  # "PRIORITY", # 1-4 # TODO determine from task priority
+            description,  # "DESCRIPTION", # Notes # determined from task notes
+            priority,  # "PRIORITY", # 1-4 # determined from task priority
             indent,  # "INDENT", # 1-4 # TODO determine from task indent
             None,  # "AUTHOR", # User
             None,  # "RESPONSIBLE", # User
-            due_date,  # "DATE", # Due Date # TODO determine from task due date; set recurring tasks if "repeat_every": true,
+            due_date,  # "DATE", # Due Date # FIXME determine from task due date; set recurring tasks if "repeat_every": true,
             "en" if due_date is not None else None,  # "DATE_LANG",
             None,  # "TIMEZONE", # UTC # TODO determine from user settings
-            None,  # "DURATION", # 0 # TODO determine from task duration
-            None,  # "DURATION_UNIT", # day # TODO determine from task duration
+            duration,  # "DURATION", # 0 # determine from task duration
+            duration_unit,  # "DURATION_UNIT", # day # determine from task duration
         ]
     )
+
+
+def parse_iso_duration(iso_str):
+    """Parse ISO 8601 duration strings to total minutes."""
+    # Regex to extract time units from ISO 8601 duration strings
+    pattern = re.compile(r"PT(?:(\d+)H)?(?:(\d+)M)?")
+    match = pattern.match(iso_str)
+    if match:
+        hours, minutes = match.groups(default="0")
+        total_minutes = int(hours) * 60 + int(minutes)
+        return total_minutes
+    return 0  # Return 0 minutes if no pattern matched
+
+
+def get_estimate_from_task(task):
+    duration = task.get("estimate", "")
+    if not duration:
+        return None, None
+
+    # Check if duration is in ISO 8601 format
+    if duration.startswith("PT"):
+        total_minutes = parse_iso_duration(duration)
+        return total_minutes, "minute" if total_minutes else (None, None)
+
+    return None, None
 
 
 def get_priority_from_task(task):
@@ -205,7 +260,124 @@ def get_priority_from_task(task):
     return None
 
 
-def write_data(writer, tasks, notes, locations, lists, tags):
+def parse_recurrence(repeat):
+    repeat_parsed = {}
+    if repeat:
+        repeat_parts = repeat.split(";")
+        for part in repeat_parts:
+            key, value = part.split("=")
+            repeat_parsed[key] = value
+
+    if repeat_parsed:
+        freq = repeat_parsed.get("FREQ", "")
+        interval = repeat_parsed.get("INTERVAL", "")
+        count = repeat_parsed.get("COUNT", "")
+        byday = repeat_parsed.get("BYDAY", "")
+        wkst = repeat_parsed.get("WKST", "")
+
+        return {
+            "freq": freq,
+            "interval": interval,
+            "count": count,
+            "byday": byday,
+            "wkst": wkst,
+        }
+
+    return None
+
+
+def date_short_to_long(day):
+    if day == "MO":
+        return "Monday"
+    if day == "TU":
+        return "Tuesday"
+    if day == "WE":
+        return "Wednesday"
+    if day == "TH":
+        return "Thursday"
+    if day == "FR":
+        return "Friday"
+    if day == "SA":
+        return "Saturday"
+    if day == "SU":
+        return "Sunday"
+    return day
+
+
+def period_short_to_long(period):
+    if period == "DAILY":
+        return "day"
+    if period == "WEEKLY":
+        return "week"
+    if period == "MONTHLY":
+        return "month"
+    if period == "YEARLY":
+        return "year"
+    return period
+
+
+def parse_due_date(due_date, date_due_has_time, repeat, repeat_every):
+    # TODO add support for date_due_has_time
+    if due_date:
+        due_date = date_from_int(due_date)
+        due_date = format_date(due_date)
+
+        if repeat:
+            """
+            parse recurring tasks if "repeat" is not null, in which case it is a semi-colon separated list of:
+            # FREQ=DAILY|MONTHLY|WEEKLY|YEARLY
+            # INTERVAL=1|2|3|4|90
+            # COUNT=4
+            # BYDAY=MO|TU|WE|TH|FR|SA|SU
+            # WKST=MO|TU|WE|TH|FR|SA|SU
+            """
+            recurrence = parse_recurrence(repeat)
+
+            if recurrence:
+                # translate into Todoist CSV task shorthand
+
+                result = []
+
+                if recurrence.get("interval"):
+                    result.append("every")
+                    if int(recurrence.get("interval") or 0) > 1:
+                        result.append(recurrence.get("interval"))
+                    if recurrence.get("byday"):
+                        result.append(date_short_to_long(recurrence.get("byday")))
+                    else:
+                        result.append(period_short_to_long(recurrence.get("freq")))
+                else:
+                    result.append(recurrence.get("freq"))
+
+                if due_date:
+                    result.append(f"starting {due_date}")
+
+                return due_date, " ".join(result)
+
+        return due_date, None
+
+    return None, None
+
+
+def get_due_date_from_task(task):
+    # "date_due": 1629842400000,
+    # "date_due_has_time": false,
+    # "repeat": "FREQ=DAILY;INTERVAL=1;WKST=SU",
+    # "repeat_every": false,
+
+    due_date = task.get("date_due", "")
+    if due_date:
+        repeat_every = task.get("repeat_every", False)
+        date_due_has_time = task.get("date_due_has_time", False)
+        repeat = task.get("repeat", "")
+        due_date, recurrence = parse_due_date(
+            due_date, date_due_has_time, repeat, repeat_every
+        )
+        return due_date, recurrence
+    return None, None
+
+
+def write_data(writer, tasks, notes, all_locations, all_lists, all_tags):
     for task in tasks:
         # Extract task details
         try:
@@ -214,32 +386,37 @@ def write_data(writer, tasks, notes, locations, lists, tags):
 
             date_created = date_from_int(task.get("date_created", ""))
             date_modified = date_from_int(task.get("date_modified", ""))
-            # indent = task.get("indent", 1)  # Default indent level
+            indent = task.get("indent", 1)  # Default indent level
 
-            due_date = date_from_int(task.get("date_due", ""))
-
-            # TODO parse "estimate" if set => DURATION,DURATION_UNIT
-
+            due_date, recurrence = get_due_date_from_task(task)
+            duration, duration_unit = get_estimate_from_task(task)
             priority = get_priority_from_task(task)
-            location = get_location_from_task(task, locations)
-            mylist = get_list_from_task(task, lists)
-            mytags = get_tags_from_task(task, tags)
+            location = get_location_from_task(task, all_locations)
+            list = get_list_from_task(task, all_lists)
+            tags = get_tags_from_task(task, all_tags)
+
+            # add labels and lists to task names
+            name = annotate_task(name, location, list, tags, recurrence)
 
             # Write task details to CSV
             task_notes = compile_notes(notes, task)
 
             # Combine the notes into a single string
-            description = get_description(task_notes)
+            description = get_description(url, task_notes)
 
-            # TODO parse "date_due_has_time" if set
-            due_date = parse_due_date(due_date)
-
-            write_tasks(writer, name, description, due_date, priority)
+            write_tasks(
+                writer,
+                name,
+                description,
+                due_date,
+                priority,
+                duration,
+                duration_unit,
+                indent,
+            )
 
             # write notes to line after task
-            write_notes(
-                writer, url, date_created, date_modified, location, mylist, mytags
-            )
+            write_notes(writer, url, date_created, date_modified, location, list, tags)
 
         except Exception as e:
             print(f"Error processing task: {e}", task)
@@ -268,20 +445,41 @@ def main(args):
             # sorting_schemes = data.get("sorting_schemes", [])
             tasks = data.get("tasks", [])
             notes = data.get("notes", [])
-            locations = data.get("locations", [])  # TODO
-            lists = data.get("lists", [])  # TODO
+            locations = data.get("locations", [])
+            lists = data.get("lists", [])
             tags = data.get("tags", [])
 
         sort_tasks(tasks)
+        # tasks = group_tasks(tasks)
+        tasks = remove_duplicates(tasks)
 
         # Split tasks based on whether 'date_completed' is present
         active_tasks = [t for t in tasks if not t.get("date_completed")]
         completed_tasks = [t for t in tasks if t.get("date_completed")]
 
         # Write active tasks to incomplete CSV
-        write_tasks_to_csv(
-            args.csv_incomplete, active_tasks, notes, locations, lists, tags
-        )
+        # loop through lists
+        # for each list, write tasks to CSV; include format_label(list name) in CSV name, before .csv
+        for lst in lists:
+            list_name = format_label(lst.get("name"))
+            csv_incomplete = f"{args.csv_incomplete}_{list_name}.csv"
+            print(f"Writing {list_name} tasks to {csv_incomplete}")
+            tasks_for_list = [
+                t for t in active_tasks if t.get("list_id") == lst.get("id")
+            ]
+            if tasks_for_list.__len__() > 0:
+                write_tasks_to_csv(
+                    csv_incomplete,
+                    tasks_for_list,
+                    notes,
+                    locations,
+                    lists=[lst],
+                    tags=tags,
+                )
+
+        # write_tasks_to_csv(
+        #     args.csv_incomplete, active_tasks, notes, locations, lists, tags
+        # )
 
         # Write completed tasks to completed CSV
         write_tasks_to_csv(
@@ -292,8 +490,55 @@ def main(args):
         print(f"Error processing files: {e}")
 
 
+def format_label(label):
+    """remove non-alphanumeric characters"""
+    # Regex pattern to match emojis
+    emoji_pattern = re.compile(
+        "["
+        "\U0001f600-\U0001f64f"  # emoticons
+        "\U0001f300-\U0001f5ff"  # symbols & pictographs
+        "\U0001f680-\U0001f6ff"  # transport & map symbols
+        "\U0001f700-\U0001f77f"  # alchemical symbols
+        "\U0001f780-\U0001f7ff"  # Geometric Shapes Extended
+        "\U0001f800-\U0001f8ff"  # Supplemental Arrows-C
+        "\U0001f900-\U0001f9ff"  # Supplemental Symbols and Pictographs
+        "\U0001fa00-\U0001fa6f"  # Chess Symbols
+        "\U0001fa70-\U0001faff"  # Symbols and Pictographs Extended-A
+        "\U00002702-\U000027b0"  # Dingbats
+        "\U000024c2-\U0001f251"
+        "]+",
+        flags=re.UNICODE,
+    )
+
+    # Remove emojis
+    label = emoji_pattern.sub("", label)
+
+    # Replace non-allowed characters with underscore
+    sanitized_label = re.sub(r"[^a-zA-Z0-9_-]+", "_", label)
+    sanitized_label = sanitized_label.strip("_")
+    return sanitized_label
+
+
+def annotate_task(name, location, list, tags, recurrence):
+    """add labels and lists to task name"""
+
+    task = " ".join(
+        [
+            name,
+            f"@{format_label(location)}" if location is not None else "",
+            f"@{format_label(list)}" if list is not None else "",
+            f"{recurrence}" if recurrence is not None else "",
+            " ".join([f"#{format_label(tag)}" for tag in tags])
+            if tags is not None
+            else "",
+        ]
+    )
+
+    return task.strip()
+
+
 def write_tasks_to_csv(file_path, tasks, notes, locations, lists, tags):
-    with open(file_path, "w", newline="") as csv_file:
+    with open(file_path, "w", newline="", encoding="utf-8") as csv_file:
         writer = csv.writer(csv_file)
         write_header(writer)
         write_data(writer, tasks, notes, locations, lists, tags)
@@ -304,12 +549,55 @@ def sort_tasks(tasks):
     tasks.sort(
         key=lambda x: (
             x.get("date_completed", 0),
+            x.get("list_id", ""),
             x.get("date_due", 0),
-            x.get("priority", "PN"),
-            x.get("name", "untitled"),
+            x.get("priority", "4"),
+            x.get("name", ""),
         ),
         reverse=True,
     )
+
+
+def remove_duplicates(tasks):
+    """
+    remove duplicate tasks
+    if task is not completed, only keep the most recent one
+    """
+    seen = set()
+    unique_tasks = []
+    for task in tasks:
+        task_name = task.get("name")
+        series_id = task.get("series_id")
+        date_completed = task.get("date_completed")
+
+        if task_name:
+            if series_id and not date_completed:
+                if series_id not in seen:
+                    seen.add(series_id)
+                    unique_tasks.append(task)
+            else:
+                unique_tasks.append(task)
+    return unique_tasks
+
+
+def group_tasks(tasks):
+    """group tasks by task name and series_id; indent = 2 for subtasks"""
+    grouped_tasks = {}
+    for task in tasks:
+        task_name = task.get("name")
+        series_id = task.get("series_id")
+        if task_name and series_id:
+            if task_name not in grouped_tasks:
+                grouped_tasks[task_name] = []
+            grouped_tasks[task_name].append(task)
+
+    # Set indent level for subsequent tasks with the same name
+    for task_name, task_list in grouped_tasks.items():
+        for i, task in enumerate(task_list):
+            if i > 0:
+                task["indent"] = 2
+
+    return tasks
 
 
 if __name__ == "__main__":
